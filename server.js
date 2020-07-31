bd098794e5ddd1e00c5712f44475fe82fc5cc3e0
Sandeep Shukla <sandeepini.2012@gmail.com>
2020-08-01 01:25:25 +0530

    Second Commit file saved in uploaded_flie

--- server.js
+++ server.js
@@ -8,13 +8,17 @@ app.get("/",(req,res,next)=>{
     res.status(200).send("Hello World");
 });
 
-app.post("/upload", function(req,res,next){
-    console.log(req.files);
-    res.send({
-        success:true,
-        message:"File Uploaded!"
+app.post("/upload", function(req, res, next) {
+    const file = req.files.photo;  //photo is simple key given from client side here we used postman there we gave photo as key.
+    file.mv('./uploaded_file/' + file.name, function(err, result) {
+     if(err) 
+      throw err;
+     res.send({
+      success: true,
+      message: "File uploaded!"
+     });
     });
-});
+   });
 
 app.listen(3000,()=>{
     console.log("Startes on port : 3000")
